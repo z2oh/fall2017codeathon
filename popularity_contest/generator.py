7ef1ec8867c8c3565baf61658d4110339f2edfb6
Jeremy Day <jadaytime@gmail.com>
2017-10-09 19:39:40 -0400

    add popularity_contest complete problem

---
+++ popularity_contest/generator.py
@@ -0,0 +1,10 @@
+import random
+
+for x in range(10):
+	print(random.randint(10,100))
+for x in range(10):
+	print(random.randint(100,1000))
+for x in range(5):
+	print(random.randint(1000,10000))
+for x in range(5):
+	print(random.randint(10**90, 10**100))
---
+++ popularity_contest/solve.py
@@ -0,0 +1,3 @@
+b = format(int(input()), 'b')
+b = b[1:] + b[0]
+print(str(int(b, 2)))
