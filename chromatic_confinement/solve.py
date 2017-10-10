6f82ce7ac765e014c93042d0fb0ae665944e339a
Jeremy Day <jadaytime@gmail.com>
2017-10-09 20:48:49 -0400

    add maybe solution for chromatic_confinement

---
+++ chromatic_confinement/solve.py
@@ -0,0 +1,79 @@
+class Tree(object):
+	def __init__(self):
+		self.left = None
+		self.right = None
+		self.dim = None
+		self.val = None
+		self.parent = None
+
+def build_kd_tree(vecs, dim, d):
+	if(len(vecs) == 0):
+		return None
+	if(len(vecs) == 1):
+		ret = Tree()
+		ret.val = vecs[0]
+		return ret
+	vals = sorted(vecs, key=lambda v: v[dim])
+	mid = int(len(vals)/2)
+	tree = Tree()
+	tree.val = vals[mid]
+	tree.dim = dim
+	tree.left = build_kd_tree(vals[:mid-1], (dim+1)%d, d)
+	if(tree.left != None):
+		tree.left.parent = tree
+	tree.right = build_kd_tree(vals[mid:], (dim+1)%d, d)
+	if(tree.right != None):
+		tree.right.parent = tree
+	return tree
+
+def dist_squared_3(A, B):
+	return (A[0] - B[0])**2 + (A[1] - B[1])**2 + (A[2] - B[2])**2
+
+(N, K) = list(map(int, input().split(' ')))
+ns = [(0,0,0)] * N
+for x in range(N):
+	ns[x] = tuple(map(int, input().split(',')))
+
+tree = build_kd_tree(ns, 0, 3)
+
+ks = [(0,0,0)] * K
+for x in range(K):
+	ks[x] = tuple(map(int, input().split(',')))
+
+def find_nearest_neighbor(root, color, top):
+	# We go down the tree to find the nearest leaf node
+	while(root.left != None and root.right != None):
+		if(color[root.dim] < root.val[root.dim]):
+			root = root.left
+		elif(color[root.dim] >= root.val[root.dim]):
+			root = root.right
+	# We set our best guess to this leaf node
+	best_guess = root.val
+	guess_dist = dist_squared_3(root.val, color)
+	while(root != top):
+		prev = root;
+		root = root.parent
+		dist = dist_squared_3(root.val, color)
+		if(dist <= guess_dist):
+			guess_dist = dist
+			best_guess = root.val
+		# If the sphere intersects with the cube of the other half of
+		# the tree we must recurse down it.
+		if(dist > (root.val[root.dim] - color[root.dim])**2):
+			other = root
+			if(root.left == prev):
+				other = root.right
+			elif(root.right == prev):
+				other = root.left
+			recurse_guess = find_nearest_neighbor(other, color, other)
+			recurse_guess_dist = dist_squared_3(recurse_guess, color)
+			if(recurse_guess_dist <= guess_dist):
+				guess_dist = recurse_guess_dist
+				best_guest = recurse_guess
+	return best_guess
+
+for x in range(K):
+	color = ks[x]
+	nn = find_nearest_neighbor(tree, color, tree)	
+	print(nn)
+
